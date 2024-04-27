63e8b5b703490a88e019ac827079b7a543be5876
Alexandre Rodrigues <btt.ale@gmail.com>
2024-04-27 19:29:17 -0300

    fix: data base file

---
+++ src/StarisApi/Repository/CharacterRepository.cs
@@ -0,0 +1,17 @@
+﻿using StarisApi.DbContexts;
+using StarisApi.Models.Characters;
+
+namespace StarisApi.Repository
+{
+    public sealed class CharacterRepository : Repository<Character>
+    {
+        public CharacterRepository(MySqlContext context) : base(context)
+        {
+        }
+
+        //public IList<Character> GetCharactersByPlanet(int planetId)
+        //{
+        //    return context.Characters.Where(x => x.PlanetId == planetId).ToList();
+        //}
+    }
+}
